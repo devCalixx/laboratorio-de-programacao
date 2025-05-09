058b3295e08e6b1f36dcb2099b63a50853f40cc6
devCalixx <julia.calixto@aluno.ifsp.edu.br>
2025-05-09 09:00:13 -0300

    Add files via upload
    plataforma
    by: Luis

---
+++ exercicio-jogo/4.py
@@ -0,0 +1,17 @@
+class Plataforma:
+    def __init__(self, nome, catalogo, quantidade_j, jogadores, comunidade,):
+        self.nome = nome
+        self.catalogo = catalogo
+        self.quantidade_j = quantidade_j
+        self.jogadores = jogadores
+        self.comunidade = comunidade
+
+plataformas = [
+    Plataforma(
+        "cAXcAX",
+        [""],
+        [""],
+        [""],
+    )
+    
+]
