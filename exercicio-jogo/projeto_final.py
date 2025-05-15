0d43c4893b4370f7f4e93d08cafabe133de78121
devCalixx <julia.calixto@aluno.ifsp.edu.br>
2025-05-15 10:47:52 -0300

    Mudanças com algumas funções funcionando

--- exercicio-jogo/projeto_final.py
+++ exercicio-jogo/projeto_final.py
@@ -46,8 +46,6 @@ jogos = [
         2
     )
 ]
-for jogo in jogos: 
-    jogo.detalhes()
 
 class Jogador:
     def __init__ (self, nickname, id_jogador, biblioteca_de_jogos, saldo_carteira, conquistas, nivel, grupo):
@@ -69,23 +67,36 @@ class Jogador:
         print(f"Grupo: {self.grupo}")
         print(f"-" * 30)              
 
-    def adicionar_jogo_biblioteca(self, jogo_procurado):
+    def adicionar_jogo_biblioteca(self, jogo_procurado, biblioteca_de_jogos):
         jogo_procurado = input("Qual jogo você quer adicionar?: ")
         for Jogo in jogos:
             if(jogo_procurado in jogos):
-                print(f"Preço do jogo: {self.preco}")
-                def debitar_saldo(self):
-                    if self.preco <= self.saldo_carteira:
-                        print(True)
-                        self.saldo_carteira -= self.preco
-                        print(f"Novo saldo: {self.saldo_carteira}")
-                        self.biblioteca_de_jogos.append(jogo_procurado)
+                if(jogo_procurado in biblioteca_de_jogos):
+                    print(f"Você já possui esse jogo.")
+                else:
+                    print(f"Preço do jogo: {self.preco}")
+                    verificar_compra = input(f"Realizar compra?\n----------\nDigite 1 para SIM\nDigite 2 para NÃO")
+                    if verificar_compra == 1:
+                        def debitar_saldo(self):
+                            if self.preco <= self.saldo_carteira:
+                                print(True)
+                                self.saldo_carteira -= self.preco
+                                print(f"Novo saldo: {self.saldo_carteira}")
+                                self.biblioteca_de_jogos.append(jogo_procurado)
+                                print(f"Novo jogo: {self.titulo}")
+                            else:
+                                print(False)
+                                print(f"Valor insuficiente para compra.")
+                    elif verificar_compra == 2:
+                        break
                     else:
-                        print(False)
-                        print(f"Valor insuficiente para compra.")
+                        print(f"Opção inválida")
         
-    def adicionar_saldo(self, valor):
-        self.saldo_carteira += valor
+    def adicionar_saldo(self, dinheiro):
+        self.dinheiro = dinheiro
+        print(f"Saldo anterior: {self.saldo_carteira}")
+        self.saldo_carteira += dinheiro
+        print(f"Novo saldo: {self.saldo_carteira}")
         
 
 jogadores = [
@@ -108,17 +119,72 @@ jogadores = [
     )
 ]
 
+class Plataforma:
+
+    def __init__(self, nome, catalogo_de_jogos, jogadores_cadastrados):
+        self.nome = nome
+        self.catalogo_de_jogos = catalogo_de_jogos
+        self.jogadores_cadastrados = jogadores_cadastrados
+
+    def adicionar_jogo(self, jogo):
+        self.catalogo_de_jogos.append(jogo)
+
+    def adicionar_jogador(self, jogador):
+        self.jogadores_cadastrados.append(jogador)
+
+    def buscar_jogo_por_nome(self, nome_jogo, catalogo_de_jogos):
+        nome_jogo = input(f"Digite o nome do jogo: ")
+        for jogo in catalogo_de_jogos:
+            if(jogo.titulo.lower() == nome_jogo.lower()): 
+                return jogo
+        return None
+
+    def buscar_jogador_por_id(self, id_jogador, jogadores_cadastrados):
+        nome_jogo = input(f"Digite o ID do usuário: ")
+        for jogador in jogadores_cadastrados:
+            if(jogador.id == id_jogador):
+                return jogador
+        return None
+
+    def listar_catalogo(self):
+        print(f"Seja bem-vindo ao catálogo de jogos de cAXcAX!\n")
+        for idx, jogo in enumerate(self.catalogo_de_jogos, start=1):
+            print(f"[{idx}]")
+            jogo.detalhes()
+            
+    catalogo = [
+        
+    ]
+
+
+
 print(f"Seja bem-vindo à cAXcAX.\nDigite seu nickname para prosseguir dentro da sua conta.")
 usuario = input("Usuário: ")
 for Jogador in jogadores:
-    if (usuario in jogadores):
+    if usuario == Jogador.nickname:
         while True:
-            opcao = input(f"O que você deseja fazer?\nExibir perfil: 1\nVer catálogo de jogos: 2\nComprar jogo: 3\nVer perfil de outro jogador: 4")
-            if (opcao = 1):
-                Jogador.detalhes()
-            elif (opcao = 2):
-            elif (opcao = 3):
-            elif (opcao = 4):
+            opcao = input(f"O que você deseja fazer?\n----------------------\nExibir perfil: 1\nVer biblioteca de jogos: 2\nComprar jogo: 3\nVer perfil de outro jogador: 4\nVer catálogo de jogos da plataforma: 5\nAdicionar saldo na carteira: 6\n-------------------\nDigite sua opção: ")
+            if opcao == "1":
+                print(f"Seu perfil\n" + "-" *30)
+                Jogador.exibir_perfil()
+            elif opcao == "2":
+                print(f"Sua biblioteca de jogos" + "-" *30)
+                Jogador.biblioteca_de_jogos()
+            elif opcao == "3":
+                Jogador.adicionar_jogo_biblioteca()
+            elif opcao == "4":
+                id_ou_nome = input(f"Busca por ID ou nome?\n1: ID\n2: nome")
+                if id_ou_nome == "1":
+                    Plataforma.buscar_jogador_por_id
+                elif id_ou_nome == "2":
+                    Plataforma.buscar_jogo_por_nome
+                else:
+                    print(f"Opção inválida")
+            elif opcao == "5":
+                Plataforma.listar_catalogo()
+            elif opcao == "6":
+                valor = float(input("Quanto quer adicionar?: "))
+                Jogador.adicionar_saldo(valor)
             else:
                 print(f"Opção não encontrada. Escolha outra opção")
     else:
